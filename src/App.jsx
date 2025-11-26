1a06992cbfecfdf7d5ab00bc35d1125eef25b9a4
Niyaz Khan <savagebaba00@gmail.com>
2025-11-26 19:03:02 +0530

    Initial React setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import StudentCard from "./components/StudentCard";
+
+function App() {
+  return (
+    <div>
+      <h1>My First React Component</h1>
+      <StudentCard />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/StudentCard.jsx
@@ -0,0 +1,11 @@
+function StudentCard() {
+  return (
+    <div>
+      <h2>Niyaz</h2>
+      <p>Frontend Developer</p>
+      <p>Skills: JS, React</p>
+    </div>
+  );
+}
+
+export default StudentCard;
