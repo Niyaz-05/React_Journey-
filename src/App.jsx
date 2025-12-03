647ea1e2cee674534779c462ec234fd91a65df1d
Niyaz Khan <savagebaba00@gmail.com>
2025-12-03 17:13:28 +0530

    ProductList and App Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,40 @@
 import StudentCard from "./components/StudentCard";
 import ProductCard from "./components/ProductCard";
-import Shoes from "./assets/image/productShoes.png"
+import Shoes from "./assets/image/productShoes.png";
 import Counter from "./components/Counter";
 import Quote from "./components/Quote";
 import Form from "./components/Form";
 import PropsChildExample from "./components/PropsChildExample";
 import ProductList from "./components/ProductList";
-function App(){
-  return(
+import { useState } from "react";
+
+function App() {
+  const [showProducts, setShowProducts] = useState(true);
+
+  return (
     <>
       <h1>Welcome</h1>
-      <StudentCard/>
-      <ProductCard 
-          productName="Shoes"
-          price="79,9"
-          rating="4.8"
-          image={Shoes}
-      /> 
-      <Counter/>
-      <Quote/>
-      <Form/>
-      <PropsChildExample/>
-      <ProductList/>
+
+      {/* Toggle Products */}
+      <button onClick={() => setShowProducts(!showProducts)}>
+        {showProducts ? "Hide Products" : "Show Products"}
+      </button>
+
+      {showProducts && <ProductList />}
+
+      <StudentCard />
+      <ProductCard
+        productName="Shoes"
+        price="79,9"
+        rating="4.8"
+        image={Shoes}
+      />
+      <Counter />
+      <Quote />
+      <Form />
+      <PropsChildExample />
     </>
-  )
+  );
 }
 
 export default App;
